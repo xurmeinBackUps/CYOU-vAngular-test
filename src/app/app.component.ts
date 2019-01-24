6f6df6df31e22d6e2991ff193b597c5a0588d97d
Xurmein <wj.pavocrist@gmail.com>
2019-01-24 16:38:25 -0500

    the first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'cyoa2';
+  title = 'How will you escape the lair of Gaba Ghoul?';
 }
